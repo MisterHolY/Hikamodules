b915c12506970d005e8b400fc7726db62aaaff2e
MisterHolY <hzdan228@gmail.com>
2024-08-14 17:32:56 +0300

    Create Musicmod.py

---
+++ Musicmod.py
@@ -0,0 +1,53 @@
+# meta developer: @MisterHolY_ebal_etot_tg
+
+
+
+from .. import loader, utils
+
+
+
+@loader.tds
+
+class MusicSreach(loader.Module):
+
+	"""Ищет Музыку🎵"""
+
+	
+
+	strings = {
+
+		"name": "MusicSreach",
+
+		"no_args": "❌ Нету аргумента",
+
+		"error": "<b>[ERROR]</b> {}"
+
+	}
+
+	
+
+	async def musiccmd(self, message):
+
+		""".music <аргумент: str>"""
+
+		try:
+
+			try:
+
+				args = message.text.split()[1]
+
+			except:
+
+				await message.edit(strings["no_args"])
+
+				return
+
+			await message.delete()
+
+			results = await message.client.inline_query("@BampiOnBot", args)
+
+			await results[0].click(message.chat_id)
+
+		except Exception as e:
+
+			await message.edit(strings.format(e))
